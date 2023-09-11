11de9f402d7b1aa3374e67b1b11a609161a782e4
Kyle Pollich <kpollich1@gmail.com>
2023-09-11 12:17:56 -0400

    add task

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
   },
-}
+};
